decf43d292b9a2bcf3f7a79b4afdcfa4d6ea6b5d
Corey Rapala <coreyrapala@gmail.com>
2021-02-25 14:33:31 -0500

    initial commit with beginning of JavaScript file

---
+++ assets/script.js
@@ -0,0 +1,68 @@
+// variable to store and loop through scheduler
+var currentDay = [
+    {
+        id: "0",
+        time12: "09",
+        time24: "09",
+        amPm: "am",
+        task: ""
+    },
+    {
+        id: "1",
+        time12: "10",
+        time24: "10",
+        amPm: "am",
+        task: ""
+    },
+    {
+        id: "2",
+        time12: "11",
+        time24: "11",
+        amPm: "am",
+        task: ""
+    },
+    {
+        id: "3",
+        time12: "12",
+        time24: "12",
+        amPm: "pm",
+        task: ""
+    },
+    {
+        id: "4",
+        time12: "01",
+        time24: "13",
+        amPm: "pm",
+        task: ""
+    },
+    {
+        id: "5",
+        time12: "02",
+        time24: "14",
+        amPm: "pm",
+        task: ""
+    },
+    {
+        id: "6",
+        time12: "03",
+        time24: "15",
+        amPm: "pm",
+        task: ""
+    },
+    {
+        id: "7",
+        time12: "04",
+        time24: "16",
+        amPm: "pm",
+        task: ""
+    },
+    {
+        id: "8",
+        time12: "05",
+        time: "17",
+        amPm: "pm",
+        task: ""
+    },
+    
+]
+
